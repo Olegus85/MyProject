830457e2f85d28ef0a6df648c2bb35454ea22640
Olegus <Olegus85@yandex.ru>
2022-03-29 19:31:40 +0300

    Добавил новое сообщение

--- src/lesson1/Homework1.java
+++ src/lesson1/Homework1.java
@@ -3,6 +3,7 @@ package lesson1;
 public class Homework1 {
     public static void main(String[] args) {
         task1();
+        System.out.println("Новое сообщение");
     }
     private static void task1(){
         System.out.println("Решение первой задачи!");
